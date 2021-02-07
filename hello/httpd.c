57eda09acd1ee147c5944ecb10d570fab2e361f5
u2i <pv3f3wii@s.okayama-u.ac.jp>
2021-02-07 15:05:17 +0900

    Update httpd.c

--- hello/httpd.c
+++ hello/httpd.c
@@ -109,3 +109,39 @@ signal_exit(int sig)
     log_exit("exit by signal %d", sig);
 }
 
+static void
+service(FILE *in, FILE *out, char *docroot)
+{
+    struct HTTPRequest *req;
+
+    req = read_request(in);
+    respond_to(req, out, docroot);
+    free_request(req);
+}
+
+static struct HTTPRequest*
+read_request(FILE *in)
+{
+    struct HTTPRequest *req;
+    struct HTTPHeaderField *h;
+
+    req = xmalloc(sizeof(struct HTTPRequest));
+    read_request_line(req, in);
+    req->header = NULL;
+    while (h = read_header_field(in)) {
+        h->next = req->header;
+        req->header = h;
+    }
+    req->length = content_length(req);
+    if (req->length != 0) {
+        if (req->length > MAX_REQUEST_BODY_LENGTH)
+            log_exit("request body too long");
+        req->body = xmalloc(req->length);
+        if (fread(req->body, req->length, 1, in) < 1)
+            log_exit("failed to read request body");
+    } else {
+        req->body = NULL;
+    }
+    return req;
+}
+
